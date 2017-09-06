ea3a3bed0ff48d2cafb93212f5f9e96d75f5c6b6
Andrea Pinillos <andreapinillos@Andreas-MacBook-Pro-2.local>
2017-09-06 09:57:43 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES
+  ("Magic Avocado Burger", true),
+  ("Double Egg Burger", false),
+  ("The Pinneaple Express Burger", true);
+
+  
+  SELECT * FROM burgers;
